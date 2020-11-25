c436cfd78080e7a5434effd490ea356b73cf4f16
EllaAmelia23 <70999839+EllaAmelia23@users.noreply.github.com>
2020-11-25 20:45:34 +0700

    Add files via upload

---
+++ PBO MINGGU12/src/Tugas/Zombie.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Zombie implements Destroyable {
+
+    protected int health;
+    protected int level;
+
+    public void heal() {
+
+    }
+
+    @Override
+    public void destroyed() {
+
+    }
+
+    public String getZombieInfo() {
+        return "";
+    }
+}
+
+
